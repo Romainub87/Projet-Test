a0480453bf7d7481deab82bc74ba5b201bf83c76
Romainub87 <romaincordier33160@gmail.com>
2024-06-26 10:51:39 +0200

    Scénario 2 fini

--- src/test/java/com/sqli/isc/iut/courses/cucumber/BarSteps.java
+++ src/test/java/com/sqli/isc/iut/courses/cucumber/BarSteps.java
@@ -8,6 +8,8 @@ import io.cucumber.java.en.When;
 public class BarSteps {
 
     private Bar bar;
+    private Man Pignon;
+    private Man Leblanc;
 
     @Given("there are only {int} seats in the bar")
     public void thereAreOnlySeatsInTheBar(int arg0) {
@@ -16,6 +18,8 @@ public class BarSteps {
 
     @And("Mr Pignon and Mr Leblanc go to the bar Le Juste")
     public void mrPignonAndMrLeblancGoToTheBarLeJuste() {
+        this.Pignon = new Man("Pignon");
+        this.Leblanc = new Man("Leblanc");
         this.bar.enter(2);
     }
 
@@ -36,31 +40,46 @@ public class BarSteps {
 
     @Then("the person behind them cannot enter because the bar is full")
     public void thePersonBehindThemCannotEnterBecauseTheBarIsFull() {
-
+        assert (this.bar.getCurrentPeople() == this.bar.getCapacity());
+        this.bar.enter(1);
+        assert (this.bar.isComplete().equals("Complete"));
+        assert (this.bar.getCurrentPeople() >= this.bar.getCapacity());
+        this.bar.suppressPeople(1);
+        assert (this.bar.getCurrentPeople() == this.bar.getCapacity());
     }
 
     @And("they each order a cocktail of the month for €{int}")
-    public void theyEachOrderACocktailOfTheMonthFor€(int arg0) {
+    public void theyEachOrderACocktailOfTheMonthFor(int arg0) {
+        this.Pignon.orderCocktails(1, arg0);
+        this.Leblanc.orderCocktails(1, arg0);
     }
 
     @And("Mr Leblanc pays for all the cocktails")
     public void mrLeblancPaysForAllTheCocktails() {
+        this.Leblanc.addToBill(this.Pignon.getBill());
+        this.Pignon.setBill(0.0);
+        this.bar.addToBill(this.Leblanc.getBill());
     }
 
     @When("they finish their drinks")
     public void theyFinishTheirDrinks() {
+        this.Pignon.drink(1);
+        this.Leblanc.drink(1);
     }
 
     @Then("the bill is checked")
     public void theBillIsChecked() {
+        assert (this.bar.getBill() == this.Leblanc.getBill());
     }
 
     @And("Mr Leblanc pays the bill")
     public void mrLeblancPaysTheBill() {
+        this.Leblanc.payBill();
     }
 
     @And("Mr Pignon is happy because they only had one drink")
     public void mrPignonIsHappyBecauseTheyOnlyHadOneDrink() {
+        assert (this.Pignon.isHappy(this.Pignon.getNumberOfDrinks()));
     }
 
 
